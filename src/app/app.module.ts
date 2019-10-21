eca2a7b41ac38cd851fd44d49f7ab1c6b7eea7a6
abolfazl <akashian4@gmail.com>
2019-10-21 22:09:24 +0330

    order style of tags and add form for add item to list and add icon for delete item (but this have bug)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {ReactiveFormsModule} from '@angular/forms'
 import { CountComponent } from './components/count/count.component';
 
 @NgModule({
@@ -13,7 +13,8 @@ import { CountComponent } from './components/count/count.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
