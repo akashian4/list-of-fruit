09900ec9a82cee1ddb13d02b0bc39c60447cf380
abolfazl <akashian4@gmail.com>
2019-11-09 13:14:18 +0330

    there is problem in routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ReactiveFormsModule} from '@angular/forms'
 import { CountComponent } from './components/count/count.component';
+import { IncrementComponent } from './increment/increment.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'count', component: CountComponent },
+  { path: 'increment', component: IncrementComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountComponent
+    CountComponent,
+    IncrementComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
